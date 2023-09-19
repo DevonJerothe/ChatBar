87d5dbf41891288e618e20e0cfcf91fd334674cb
Devon Jerothe <devonjerothe@gmail.com>
2023-09-19 17:46:11 -0400

    Initial Commit

---
+++ chatbar/chatbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatbarApp.swift
+//  chatbar
+//
+//  Created by devon jerothe on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct chatbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
